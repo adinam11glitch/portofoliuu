1ab48b2f4b444515398ce65d5d5b5a89b183a34d
adinam11glitch <claudiazaldoiu11@gmail.com>
2025-11-13 17:26:59 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+//Cche the DOM
+var button = document.getElementById("button")
+var nume = document.getElementById("nume")
+var prenume = document.getElementById("prenume")
+var img = document.querySelector("img")
+var body = document.querySelector("body")
+var educatie = document.getElementById("educatie")
+var asteptari = document.getElementById("asteptari")
+var an=document.getElementById("anNastere")
+
+//Add event listener
+button.addEventListener("click", altaViata)
+an.addEventListener("mouseover", displayAge)
+
+const d=new Date()
+var year=d.getFullYear()
+
+//Define function
+
+function displayAge(){
+
+	an.innerHTML=year-an.innerHTML
+
+}
+
+
+function altaViata() {
+	nume.innerHTML = "Software Engineer"
+	prenume.innerHTML = "Google"
+	
+	educatie.innerHTML = "Experiente"
+	asteptari.innerHTML = "<ul><li>Workshops</li><li>Internships</li></ul>"
+	img.src = "images/caty.jpg"
+	img.style.opacity = "70%"
+	img.style.height = "300px"
+	img.style.width = "400px"
+	img.style.border = "3px solid black"
+	
+	body.style.backgroundColor = "Yellow"
+}
